ddaf9d62c8e978110a74867688eee27bf68868bd
Maxime <maximechagnon00@gmail.com>
2024-10-03 18:27:33 +0200

    feat(main): tests

--- TP1+2/main.py
+++ TP1+2/main.py
@@ -1,5 +1,6 @@
 from exercice1.chiffre import chiffreVigenere
 from exercice2.dechiffre import dechiffreVigenere
+from exercice3.kasiski import kasiski
 
 if __name__ == "__main__":
     texte_clair = "Bonjour"
@@ -8,3 +9,20 @@ if __name__ == "__main__":
     texte_chiffre = chiffreVigenere(texte_clair, cle)
     print(texte_chiffre)
     print(dechiffreVigenere(texte_chiffre, cle))
+
+    texte_clair = "Je suis un chat rouge"
+    cle = "FRAISE"
+
+    texte_chiffre = chiffreVigenere(texte_clair, cle)
+    print(texte_chiffre)
+    print(dechiffreVigenere(texte_chiffre, cle))
+
+    texte_clair = "La cle n'existe pas"
+    cle = ""
+
+    texte_chiffre = chiffreVigenere(texte_clair, cle)
+    print(texte_chiffre)
+    print(dechiffreVigenere(texte_chiffre, cle))
+
+    texte_chiffre = "kasiski.txt"
+    print(kasiski(texte_chiffre))
