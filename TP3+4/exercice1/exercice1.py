602d74d6772feea27bec21f34c970607a21c705d
Maxime <maximechagnon00@gmail.com>
2024-10-10 15:27:52 +0200

    feat: exercice1

---
+++ TP3+4/exercice1/exercice1.py
@@ -0,0 +1,25 @@
+from hashlib import sha256
+
+def password_generate(master_password : str, tag : str) -> str:
+    sha256_password: str
+    res : str
+    legal_character : int
+
+    res = ""
+    legal_character = 0
+
+    sha256_password =  sha256(master_password.encode() + tag.encode()).hexdigest()
+
+    for i in range(0, 8):
+        char = sha256_password[8*i:8*i+8]
+        for character in char:
+            legal_character += ord(character)
+        legal_character = (legal_character - 33) % 95 + 33
+
+        res += chr(legal_character)
+
+    return res
+
+
+if __name__ == "__main__":
+    print(password_generate("lemotdepasse", "Unilim"))
