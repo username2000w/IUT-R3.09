e950ca3d2be4479cffa904877b3025b11a9e2a97
Maxime <maximechagnon00@gmail.com>
2024-10-10 15:28:00 +0200

    feat: exercice 2

---
+++ TP3+4/exercice2/exercice2.py
@@ -0,0 +1,26 @@
+from hashlib import sha512
+
+def password_generate(master_password : str, tag : str, size : int) -> str:
+    sha256_password: str
+    res : str
+    legal_character : int
+    cutting_size : int
+
+    res = ""
+    legal_character = 0
+    sha256_password =  sha512(master_password.encode() + tag.encode()).hexdigest()
+    cutting_size = len(sha256_password)//size
+
+    for i in range(0, size):
+        char = sha256_password[cutting_size*i:cutting_size*i+cutting_size]
+        for character in char:
+            legal_character += ord(character)
+        legal_character = (legal_character - 33) % 95 + 33
+
+        res += chr(legal_character)
+
+    return res
+
+
+if __name__ == "__main__":
+    print(password_generate("lemotdepasse", "Unilim", 10))
