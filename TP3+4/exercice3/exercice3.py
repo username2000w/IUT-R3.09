7a792d019c52b84b0ba3daa1c1a2366762680856
Maxime <maximechagnon00@gmail.com>
2024-10-15 16:03:56 +0200

    feat: exercice 3

---
+++ TP3+4/exercice3/exercice3.py
@@ -0,0 +1,41 @@
+from hashlib import sha512
+import os
+
+def password_generate(tag : str, size : int) -> str:
+    sha256_password: str
+    res : str
+    legal_character : int
+    cutting_size : int
+
+    res = ""
+    legal_character = 0
+
+    file = os.path.join(os.path.dirname(__file__), "mpwd.txt")
+    try:
+        with open(file, "r") as f:
+            master_password = f.read()
+            f.close()
+    except FileNotFoundError:
+        with open(file, "w") as f:
+            f.write("MotDePasse")
+            f.close()
+        with open(file, "r") as f:
+            master_password = f.read()
+            f.close()
+
+    sha256_password =  sha512(master_password.encode() + tag.encode()).hexdigest()
+    cutting_size = len(sha256_password)//size
+
+    for i in range(0, size):
+        char = sha256_password[cutting_size*i:cutting_size*i+cutting_size]
+        for character in char:
+            legal_character += ord(character)
+        legal_character = (legal_character - 33) % 95 + 33
+
+        res += chr(legal_character)
+
+    return res
+
+
+if __name__ == "__main__":
+    print(password_generate("Unilim", 10))
