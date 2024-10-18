f44db7e9d7bd5f1b39795fec98c40dc9fdb2413c
Maxime <maximechagnon00@gmail.com>
2024-10-18 11:45:24 +0200

    fix(ex1): use `size`

--- TP3+4/exercice1/exercice1.py
+++ TP3+4/exercice1/exercice1.py
@@ -11,9 +11,10 @@ def password_generate(master_password : str, tag : str) -> str:
     legal_character = 0
 
     sha512_password =  sha512(master_password.encode() + tag.encode()).hexdigest()
+
     cutting_size = len(sha512_password)//size
 
-    for i in range(0, 8):
+    for i in range(0, size):
         char = sha512_password[cutting_size*i:cutting_size*i+cutting_size]
         for character in char:
             legal_character += ord(character)
